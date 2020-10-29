ea22d24199d341f51445f902da5e952b94847553
Nate Berkopec <nate.berkopec@gmail.com>
2020-10-29 10:50:56 -0600

    Change license key email upfront, fail

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -12,8 +12,20 @@ class LicensesController < ApplicationController
   end
 
   def update
-    EmailKeyRegistrationJob.perform_later(params[:key], params[:email])
-    head :ok
+    license_key = LicenseKey.find_by(key: params[:key])
+    license_key.with_lock do
+      return if license_key.email == params[:email].downcase || license_key.email_changed
+      license_key.email_changed = true
+      license_key.email = params[:email].downcase
+      license_key.save
+    end
+
+    if license_key.changed? && !license_key.errors.any?
+      EmailKeyRegistrationJob.perform_later(params[:key], params[:email])
+      head :ok
+    else
+      head :forbidden
+    end
   end
 
   def destroy
--- app/jobs/email_key_registration_job.rb
+++ app/jobs/email_key_registration_job.rb
@@ -2,14 +2,6 @@ class EmailKeyRegistrationJob < ActiveJob::Base
   ZAPIER_HOOK_URL = ENV["ZAPIER_HOOK_URL"]
 
   def perform(key, email)
-    license_key = LicenseKey.find_by(key: key)
-    license_key.with_lock do
-      return if license_key.email == email.downcase || license_key.email_changed
-      license_key.email_changed = true
-      license_key.email = email.downcase
-      license_key.save!
-    end
-
     Typhoeus::Request.post(
       ZAPIER_HOOK_URL,
       body: {
